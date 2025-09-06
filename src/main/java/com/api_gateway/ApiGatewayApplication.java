3afcc02b4474baf77ea07c9b14de094a275a43c3
Marcinho <marcin1897@gmail.com>
2025-09-06 15:40:40 +0200

    Circuit breaker fix

--- src/main/java/com/api_gateway/ApiGatewayApplication.java
+++ src/main/java/com/api_gateway/ApiGatewayApplication.java
@@ -4,13 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication(exclude = {
 		SecurityAutoConfiguration.class,
 		ManagementWebSecurityAutoConfiguration.class
 })
-@EnableCaching
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
